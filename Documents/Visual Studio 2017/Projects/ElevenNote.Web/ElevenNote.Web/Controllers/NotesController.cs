333be170aff64fe93dfb8bbd858c42655460c08b
Shelby Grice <Shelby Grice>
2017-04-12 15:42:23 -0400

    Created note create model, view, and action

--- Documents/Visual Studio 2017/Projects/ElevenNote.Web/ElevenNote.Web/Controllers/NotesController.cs
+++ Documents/Visual Studio 2017/Projects/ElevenNote.Web/ElevenNote.Web/Controllers/NotesController.cs
@@ -15,5 +15,21 @@ namespace ElevenNote.Web.Controllers
             var model = new NoteListItem[0];
             return View(model);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
     }
 }
