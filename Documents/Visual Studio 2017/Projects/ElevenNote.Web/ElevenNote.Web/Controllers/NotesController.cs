c7883c377229a992e84d0e0b006c1a17f7753064
Shelby Grice <Shelby Grice>
2017-04-13 11:03:45 -0400

    Provided user feedback

--- Documents/Visual Studio 2017/Projects/ElevenNote.Web/ElevenNote.Web/Controllers/NotesController.cs
+++ Documents/Visual Studio 2017/Projects/ElevenNote.Web/ElevenNote.Web/Controllers/NotesController.cs
@@ -14,15 +14,14 @@ namespace ElevenNote.Web.Controllers
     {
         public ActionResult Index()
         {
-            var userId = Guid.Parse(User.Identity.GetUserId());
-            var service = new NoteService(userId);
+            var service = CreateNoteService();
             var model = service.GetNotes();
 
             return View(model);
         }
 
         public ActionResult Create()
-        {
+        { 
 
             return View();
         }
@@ -33,12 +32,23 @@ namespace ElevenNote.Web.Controllers
         {
             if (!ModelState.IsValid) return View(model);
 
+            var service = CreateNoteService();
+
+            if(service.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was created.";    
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your note could not be created.");
+            return View(model);
+        }
+
+        private NoteService CreateNoteService()
+        {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new NoteService(userId);
-
-            service.CreateNote(model);
-
-            return RedirectToAction("Index");
+            return service;
         }
     }
 }
